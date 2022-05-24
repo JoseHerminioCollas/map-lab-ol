35e10813c559d9734a72ac2db4d84ad43ab6de31
Jose Collas <joseherminiocollas@goatstone.com>
2022-05-23 20:09:27 -0500

    add tests

--- src/__tests__/gibs-vis.test.ts
+++ src/__tests__/gibs-vis.test.ts
@@ -1,4 +1,4 @@
-import gibsVis from 'control/GIBSVis';
+import gibsVis, { Day } from 'control/GIBSVis';
 
 const id = 'MODIS_Terra_CorrectedReflectance_TrueColor'
 const id2 = 'VIIRS_SNPP_CorrectedReflectance_TrueColor'
@@ -7,7 +7,7 @@ describe("GIBSVis", () => {
         const value = Object.values(gibsVis.getAll());
         expect(value.length).toBeGreaterThan(1);
     });
-    test("GIBSVis sets and listens the Visual value", () => {
+    test("GIBSVis sets and listens to the Visual value", () => {
         gibsVis.setVis(id)
         return new Promise((res, rej) => {
             gibsVis.listenVis((v) => {
@@ -17,14 +17,23 @@ describe("GIBSVis", () => {
             expect(id).toBe(v)
         })
     })
+    test('setVis and getVis should have the same value', () => {
+        gibsVis.setVis(id)
+        expect(gibsVis.getVis().identifier).toBe(id)
+    })
+    test('setDay and getDay have the same value', () => {
+        const expectedDay: Day = [2022, 1, 1]
+        gibsVis.setDay(expectedDay)
+        expect(gibsVis.getDay()).toBe([expectedDay])
+    })
 })
 
-// getVis: GetVis
-// listenVis: ListenVis
-// setVis: Set
-// setDay: SetDay
-// getDay: GetDay
-// getAll: GetAll
+// getVis: GetVis XXX
+// listenVis: ListenVis XXX
+// setVis: Set XXX
+// setDay: SetDay XXX
+// getDay: GetDay XXX
+// getAll: GetAll XXX
 // getSourceUrl: GetSourceUrl
 // getMinMax: GetMinMax
 // listenSourceUrl: ListenSourceUrl
